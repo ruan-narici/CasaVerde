ee48ca687ae1ebd983e4f251d706ea028fc7db72
ruan-narici <ruannarici@hotmail.com>
2024-07-31 21:58:12 -0300

    style: ADD style for list of navbar

--- src/components/Header/NavBar/index.tsx
+++ src/components/Header/NavBar/index.tsx
@@ -1,9 +1,10 @@
 import { Link } from 'react-router-dom';
+import stlyes from './NavBar.module.scss';
 
 function NavBar() {
   return (
     <nav>
-      <ul>
+      <ul className={ stlyes.list }>
         <li>Como fazer</li>
         <li>Ofertas</li>
         <li>Depoimentos</li>
